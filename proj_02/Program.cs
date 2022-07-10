07d522ee55f13d3cd404c6e23b97b28b8adf30d6
Francisko <errasti007@yandex.ru>
2022-07-10 17:31:55 +0300

    Initial commit for square project

---
+++ proj_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Это программа проверяет является ли первое число, квадратом второго");
+Console.WriteLine("Введите первое число: ");
+int firstnum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secnum = int.Parse(Console.ReadLine());
+
+if (firstnum / secnum == secnum )
+{
+    Console.WriteLine("Это квадрат второго числа!");
+}
+else
+{
+    Console.WriteLine("Это не квадрат второго числа!");
+}
