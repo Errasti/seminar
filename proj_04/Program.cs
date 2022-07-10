58aadbaa2bb1eae801f9ceae12247130682febaf
Francisko <errasti007@yandex.ru>
2022-07-10 18:51:18 +0300

    N numbers proj

---
+++ proj_04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+/*
+int i = -num;
+while (i <= num)
+{
+    Console.Write(i + " ");
+    i = i + 1;
+}
+*/
+
+for (int i = -num; i <= num; i++)
+{
+    Console.Write(i + " ");
+}
