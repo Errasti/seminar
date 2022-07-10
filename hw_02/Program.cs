4f05136ca921d0fce669c83d53140f7fd0f28be9
Francisko <errasti007@yandex.ru>
2022-07-10 20:51:36 +0300

    Initial commit for second hometask

---
+++ hw_02/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите первое число: ");
+int firstnum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondnum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdnum = int.Parse(Console.ReadLine());
+int max = firstnum;
+
+if ( max < secondnum )
+{
+    max = secondnum;
+}
+else if ( max < thirdnum )
+{
+    max = thirdnum;
+}
+
+Console.WriteLine("Максимальное число равно: " + max);
+
