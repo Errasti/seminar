565919f072616a736ab5864f7a8304046ac11a4e
Francisko <errasti007@yandex.ru>
2022-07-10 18:19:08 +0300

    Initial commit day of the week project

---
+++ proj_03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+if (n == 1)
+{
+    Console.WriteLine("Понедельник");
+} 
+else if (n == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+else if (n == 3) 
+{
+    Console.WriteLine("Среда");
+}
+else if (n == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+else if (n == 5) 
+{
+    Console.WriteLine("Пятница");
+}
+else if (n == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+else if (n == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
